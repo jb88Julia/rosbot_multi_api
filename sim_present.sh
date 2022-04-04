f4cd866fe81995f45efe1a5a060f5537fc4d6dcb
jb88Julia <juulia.bonat@gmail.com>
2022-04-04 14:23:53 +0100

    separate files for presentation

---
+++ sim_present.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Launching simulation essentials..."
+# initializes simulation world, spawns robots, starts map server, amcl and movebase:
+sh ./sim_nav_base.sh
+sleep 1
+#starts Mission Control server:
+#echo "Launching presentation sequence ..."
+#gnome-terminal -- roslaunch rosbot_multi_api .launchsim_presentation
